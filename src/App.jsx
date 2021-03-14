8272d74422a4933da09ba0570fde52f9cf292831
José Caldas <jrcaldas@yahoo.com.br>
2021-03-13 21:08:44 -0300

    refactor components

--- src/App.jsx
+++ src/App.jsx
@@ -1,173 +1,38 @@
 import styled from "styled-components";
-import React, { useState } from "react";
-import { VscChromeClose } from "react-icons/vsc";
-import { FcCalendar } from "react-icons/fc";
 import { MdKeyboard } from "react-icons/md";
-
 import "./Global.css";
-import { Modal } from "./components/Modal";
+import Modal from "./components/Modal";
 
 function App() {
-  const [task, setTask] = useState("");
-  const [taskList, setTaskList] = useState([]);
-
-  const onChangeHandler = (event) => {
-    setTask(event.target.value);
-  };
-
-  const addTask = () => {
-    if (task !== "") {
-      const taskDetail = {
-        id: Math.floor(Math.random() * 1000),
-        value: task,
-        isCompleted: false,
-      };
-      setTaskList([...taskList, taskDetail]);
-    }
-  };
-
-  const deleteTask = (e, id) => {
-    e.preventDefault();
-    setTaskList(taskList.filter((t) => t.id !== id));
-  };
-
-  const taskCompleted = (e, id) => {
-    e.preventDefault();
-    const element = taskList.findIndex((elem) => elem.id === id);
-    const newTaskList = [...taskList];
-    newTaskList[element] = {
-      ...newTaskList[element],
-      isCompleted: true,
-    };
-    setTaskList(newTaskList);
-  };
-
-  const [isModalVisible, setModalVisible] = useState(false);
-
   return (
-    <Container>
-      <Content>
+    <div className="App">
+      <AppContainer>
         <MdKeyboard className="keyboard" />
-        <header>
+        <Header>
           <small>Created by</small>
           <strong>José Caldas</strong>
-        </header>
+        </Header>
         <h1>Keep productivity with DoList </h1>
         <h2>Tasks </h2>
-        <Navtabs>
-          <div>
-            <TabItem isActive={task === "Todo"} onClick={addTask}>
-              <p>To-do</p>
-            </TabItem>
-            {/* <TabItem isActive={task === "All"} onClick={() => setTask("All")}>
-              <p>All</p>
-            </TabItem>
-            <TabItem isActive={task === "Done"} onClick={() => setTask("Done")}>
-              <p>Done</p>
-            </TabItem> */}
-          </div>
 
-          <div>
-            <input
-              type="text"
-              name="name"
-              placeholder="new todo..."
-              onChange={(e) => onChangeHandler(e)}
-              value={task}
-            ></input>
-            <button onClick={addTask} className="add">
-              Add ToDo
-            </button>
-          </div>
-        </Navtabs>
-
-        <ContentTabs>
-          {taskList !== [] ? (
-            <article>
-              {taskList.map((t) => (
-                <label className="contentArticle">
-                  <div>
-                    <input
-                      className="completed"
-                      type="checkbox"
-                      name="done"
-                      id="done"
-                      onClick={(e) => taskCompleted(e, t.id)}
-                    />
-                    <div className="completeTask">
-                      <p className={t.isCompleted ? "crossText" : "listItem"}>
-                        {t.value}
-                      </p>
-                    </div>
-                  </div>
-                  <div className="nav-left">
-                    <small>{Date()}</small>
-                    <span>
-                      <FcCalendar />
-                    </span>
-                    <button
-                      className="delete"
-                      onClick={(e) => deleteTask(e, t.id)}
-                    >
-                      <VscChromeClose />
-                    </button>
-                  </div>
-                </label>
-              ))}
-            </article>
-          ) : null}
-
-          {/* <article>{task === "All" && "All"}</article>
-          <article>{task === "Done" && "Done"}</article> */}
-        </ContentTabs>
-        <Button onClick={() => setModalVisible(true)}>+</Button>
-        {isModalVisible ? (
-          <Modal onClose={() => setModalVisible(false)}></Modal>
-        ) : null}
-      </Content>
-    </Container>
+        <Modal />
+      </AppContainer>
+    </div>
   );
 }
 
 export default App;
 
-export function ModalB() {}
-
-const Container = styled.div`
+const AppContainer = styled.div`
   width: 100%;
   height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-`;
-const Content = styled.div`
-  width: 100%;
   max-width: 800px;
   display: flex;
+  justify-content: space-around;
   min-height: 600px;
   flex-direction: column;
   margin: 0 auto;
-  justify-content: space-around;
   padding: 0 10px;
-
-  header small {
-    color: var(--gray100);
-    font-size: 18px;
-  }
-  header {
-    display: flex;
-    align-items: center;
-  }
-  header strong {
-    width: 240px;
-    border: 3px solid #eae9f2;
-    border-radius: 34px;
-    padding: 10px 10px;
-    margin-left: 20px;
-    font-size: 18px;
-    color: var(--gray);
-    text-align: center;
-  }
   h1 {
     color: var(--blue);
     font-size: 42px;
@@ -179,115 +44,22 @@ const Content = styled.div`
     font-weight: bold;
   }
 `;
-
-const Navtabs = styled.div`
-  width: 100%;
+const Header = styled.div`
   display: flex;
-  justify-content: space-around;
+  align-items: center;
 
-  input {
-    border: none;
-    border-bottom: 2px solid var(--blue);
-  }
-
-  button {
-  }
-  button p {
+  small {
+    color: var(--gray100);
     font-size: 18px;
-    font-weight: bold;
+  }
+  strong {
+    width: 240px;
+    border: 3px solid #eae9f2;
+    border-radius: 34px;
+    padding: 10px 10px;
+    margin-left: 20px;
+    font-size: 18px;
     color: var(--gray);
-    cursor: pointer;
-    position: relative;
-    padding: 0 10px;
+    text-align: center;
   }
-
-  button p:hover {
-    transition: all 0.3s ease;
-  }
-  button p:before {
-    content: "";
-    position: absolute;
-    width: 0;
-    height: 2px;
-    background: var(--blue);
-    left: 0;
-    bottom: 0;
-    transition: all 0.3s ease;
-  }
-  p:hover::before {
-    width: 100%;
-    transition: all 0.3s ease;
-  }
-
-  .add {
-    border: 1px solid var(--gray200);
-    color: var(--blue);
-    font-weight: bold;
-    margin-left: 5px;
-    padding: 5px;
-    border-radius: 5px;
-  }
-`;
-
-const TabItem = styled.button`
-  border-bottom: ${(props) => (props.isActive ? " 1px solid var(--blue)" : "")};
-`;
-
-const ContentTabs = styled.div`
-  article {
-    color: var(--gray200);
-    width: 100%;
-    height: 200px;
-    overflow-y: auto;
-  }
-
-  article div {
-    display: flex;
-    align-items: center;
-  }
-
-  .contentArticle {
-    display: flex;
-    justify-content: space-between;
-  }
-
-  .contentArticle p {
-    color: var(--gray200);
-    margin-left: 10px;
-  }
-
-  .contentArticle button {
-    margin: 0 20px;
-    color: red;
-  }
-
-  .nav-left {
-  }
-  .nav-left span {
-    margin-left: 10px;
-  }
-  .completeTask {
-  }
-
-  .nav-left svg {
-    display: block;
-    color: red;
-  }
-  .crossText {
-    text-decoration: line-through;
-    font-style: italic;
-    color: var(--blue);
-  }
-`;
-
-const Button = styled.div`
-  margin: 0 auto;
-  cursor: pointer;
-  font-size: 40px;
-  background: var(--blue);
-  width: 50px;
-  height: 50px;
-  border-radius: 50%;
-  color: var(--white);
-  text-align: center;
 `;
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,91 +1,323 @@
-import styled from "styled-components";
 import React, { useState } from "react";
+import styled from "styled-components";
 import { FiPlus } from "react-icons/fi";
-export function Modal({ id = "modal", onClose = () => {} }) {
+import { MdClose } from "react-icons/md";
+import { FcCalendar } from "react-icons/fc";
+// import { BiSquareRounded } from "react-icons/bi";
+
+function Modal({ id = "close" }) {
+  //Modal ==========================================================================
+  const [isModalVisible, setModalIsvisible] = useState(false);
   const handleOutSideClick = (e) => {
-    if (e.target.id === id) onClose();
+    if (e.target.id === "close") setModalIsvisible(false);
+    console.log(e.target.value);
   };
 
-  const [inputValue, setInputValue] = useState("");
-  const onChangeHandler = (event) => {
-    setInputValue(event.target.value);
+  //Todo =============================================================================
+
+  const [task, setTask] = useState("");
+  const [taskList, setTaskList] = useState([]);
+  const inputTextHandler = (e) => {
+    setTask(e.target.value);
+  };
+
+  const addTask = () => {
+    if (task !== "") {
+      const taskDetail = {
+        id: Math.floor(Math.random() * 1000),
+        value: task,
+        isCompleted: false,
+      };
+
+      setTaskList([...taskList, taskDetail]);
+    }
+  };
+
+  const deleteTask = (e, id) => {
+    e.preventDefault();
+    setTaskList(taskList.filter((t) => t.id !== id));
+  };
+
+  const taskCompleted = (e, id) => {
+    e.preventDefault();
+    const element = taskList.findIndex((elem) => elem.id === id);
+    const newTaskList = [...taskList];
+    newTaskList[element] = {
+      ...newTaskList[element],
+      isCompleted: true,
+    };
+    setTaskList(newTaskList);
   };
 
   return (
-    <Wrapper onClick={handleOutSideClick} className="modal" id={id}>
-      <Container>
-        <Content>
-          <h1>Write down your goal</h1>
-          <footer>
-            <input
-              type="text"
-              name="name"
-              onChange={onChangeHandler}
-              value={inputValue}
-            />
-            {/* <p>{inputValue}</p> */}
-            <button onClick={onClose}>
-              <FiPlus />
-            </button>
-          </footer>
-        </Content>
-      </Container>
-    </Wrapper>
+    <div>
+      {isModalVisible ? (
+        <ModalContainer onClick={handleOutSideClick} id={id}>
+          <div className="content">
+            <div className="modal">
+              <header>
+                <h1>Write down your goal</h1>
+              </header>
+              <footer>
+                <input
+                  onChange={(e) => inputTextHandler(e)}
+                  type="text"
+                  name=""
+                  id=""
+                />
+
+                <button className="add-btn" onClick={addTask}>
+                  <FiPlus />
+                </button>
+                <button
+                  className="close-modal"
+                  onClick={() => setModalIsvisible(false)}
+                >
+                  OK
+                </button>
+              </footer>
+            </div>
+          </div>
+        </ModalContainer>
+      ) : null}
+
+      <NavContainer>
+        <Titles>
+          <p>todo</p>
+          <p>All</p>
+          <p>Done</p>
+        </Titles>
+        <NavContent>
+          {taskList !== [] ? (
+            <article>
+              {taskList.map((t) => (
+                <label className="contentArticle">
+                  <div>
+                    <input
+                      className="completed"
+                      type="checkbox"
+                      name="done"
+                      id="done"
+                      onClick={(e) => taskCompleted(e, t.id)}
+                    />
+                    <div className="completeTask">
+                      <p className={t.isCompleted ? "crossText" : "listItem"}>
+                        {t.value}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="nav-left">
+                    <small>{Date()}</small>
+                    <span>
+                      <FcCalendar />
+                    </span>
+                    <button
+                      className="delete"
+                      onClick={(e) => deleteTask(e, t.id)}
+                    >
+                      <MdClose />
+                    </button>
+                  </div>
+                </label>
+              ))}
+            </article>
+          ) : null}
+        </NavContent>
+      </NavContainer>
+
+      <OpenModalButton onClick={() => setModalIsvisible(true)}>
+        +
+      </OpenModalButton>
+    </div>
   );
 }
 
-const Wrapper = styled.div`
+export default Modal;
+
+const ModalContainer = styled.div`
   display: flex;
-  justify-content: center;
   align-items: center;
+  justify-content: center;
   width: 100%;
   height: 100vh;
-
-  background: rgba(0, 0, 0, 0.5);
-
+  position: absolute;
   top: 0;
   left: 0;
-  position: absolute;
   z-index: 10;
-`;
+  background: rgba(0, 0, 0, 0.5);
 
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
+  .content {
+    display: flex;
+    justify-content: center;
+    background: var(--white);
+    min-width: 50%;
+    height: 25%;
+    border-radius: 5px;
+  }
 
-  background: var(--white);
-  width: 70%;
-  height: 30%;
-  border-radius: 5px;
-`;
-const Content = styled.div`
-  width: 80%;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-
-  h1 {
+  header h1 {
+    font-size: 30px;
     text-align: center;
   }
 
-  footer {
+  .modal {
+    width: 80%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+
+    footer {
+      display: flex;
+      justify-content: space-between;
+    }
+
+    input {
+      width: 100%;
+      border: none;
+      border-bottom: 1px solid var(--blue);
+      font-size: 18px;
+      color: var(--blue);
+      background: transparent;
+    }
+    button.add-btn {
+      border: none;
+      background: none;
+      border-bottom: 1px solid var(--blue);
+      cursor: pointer;
+    }
+    button.add-btn svg {
+      color: var(--blue);
+      width: 30px;
+      height: 30px;
+    }
+    button.close-modal {
+      background-color: var(--blue);
+      padding: 0 10px;
+      border-radius: 5px;
+      color: var(--white);
+      cursor: pointer;
+    }
+    button.close-modal:hover {
+      opacity: 0.8;
+    }
+  }
+`;
+
+const OpenModalButton = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+  cursor: pointer;
+  font-size: 40px;
+  background: var(--blue);
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  color: #fff;
+  text-align: center;
+  margin-top: 20px;
+`;
+
+const NavContainer = styled.div`
+  width: 100%;
+  height: 200px;
+  display: flex;
+  align-items: center;
+
+  flex-direction: column;
+`;
+
+const Titles = styled.div`
+  width: 60%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  margin-bottom: 20px;
+
+  p {
+    cursor: pointer;
+    font-size: 18px;
+    font-weight: bold;
+    color: var(--gray);
+    position: relative;
+  }
+
+  p:hover {
+    transition: all 0.3s ease;
+  }
+  p:before {
+    content: "";
+    position: absolute;
+    width: 0;
+    height: 2px;
+    background: var(--blue);
+    left: 0;
+    bottom: 0;
+    transition: all 0.3s ease;
+  }
+  p:hover::before {
+    width: 100%;
+    transition: all 0.3s ease;
+  }
+`;
+const NavContent = styled.div`
+  width: 80%;
+  article {
+    color: var(--gray200);
+    width: 100%;
+    height: 150px;
+    overflow-y: auto;
+  }
+
+  article div {
+    display: flex;
+    align-items: center;
+  }
+
+  .contentArticle {
     display: flex;
     justify-content: space-between;
   }
-  input {
-    width: 100%;
-    border: none;
-    border-bottom: 1px solid var(--blue);
+
+  p {
+    color: var(--gray200);
+    margin-left: 12px;
+  }
+
+  small {
+    font-size: 10px;
+  }
+
+  .contentArticle button {
+    margin: 0 20px;
+    color: red;
+  }
+
+  .nav-left {
+  }
+  .nav-left span {
+    margin-left: 10px;
+  }
+  .completeTask p {
+    color: var(--gray);
     font-size: 18px;
   }
-  button {
-    border: none;
-    background: none;
-    border-bottom: 1px solid var(--blue);
+
+  .nav-left svg {
+    display: block;
   }
-  svg {
-    color: var(--blue);
-    width: 30px;
-    height: 30px;
+  .crossText {
+    text-decoration: line-through;
+    font-style: italic;
+    color: var(--gray);
+  }
+
+  .listItem p {
+    font-size: 18px;
+    color: blue;
+  }
+  .delete {
+    background: none;
   }
 `;
